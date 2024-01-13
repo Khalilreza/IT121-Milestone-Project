28e1993130e534e5b80769ae8e2317dfa51f3f88
Khalil Reza <reza21khalil@gmail.com>
2024-01-13 15:58:03 -0800

    My First Commit

---
+++ greetings.js
@@ -0,0 +1,2 @@
+/*Create an alert using JS that fulfilles the required Milestine assignment.*/
+alert("Hi there, Dr.Martins! I hope you find this alert comment with ease! It is us, Khalil Reza, Val Munguia, and Elizabeth Nichols");
